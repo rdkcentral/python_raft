a2deefeae6b2fe5684a40f1cae49091cbc716c6a
Toby Baldock <toby.baldock@sky.uk>
2024-04-29 16:51:34 +0100

    gh30 - Corrected unittest files paths
    https://github.com/rdkcentral/python_raft/issues/30

--- tests/powerSwitch_tests.py
+++ tests/powerSwitch_tests.py
@@ -4,7 +4,7 @@ import sys
 
 # Add the framework path to system
 dir_path = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(dir_path+"/../../")
+sys.path.append(dir_path+"/../")
 
 from framework.core.powerControl import powerControlClass
 from framework.core.rackController import rackController
