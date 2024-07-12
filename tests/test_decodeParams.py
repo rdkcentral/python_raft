cfdc4d315142350ca87dc738e2114269bef580be
zghp <FKC01@bw-hermes.brwswteam.co.uk>
2024-07-12 08:48:45 +0100

    refactor code

--- tests/test_decodeParams.py
+++ tests/test_decodeParams.py
@@ -3,7 +3,6 @@
 import sys
 import os
 import unittest
-import argparse
 
 # Add the directory containing the framework package to the Python path
 path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
@@ -11,7 +10,7 @@ sys.path.append(path)
 
 from framework.core.singleton import Singleton, RAFTUnitTestCase
 
-singleton = Singleton(log=None)
+SINGLETON = Singleton(log=None)
 
 
 class TestDecodeParams(RAFTUnitTestCase):
@@ -19,20 +18,7 @@ class TestDecodeParams(RAFTUnitTestCase):
     def test_decodeParams(self):   
         print("Hello World")
 
-def main():
-    parser = argparse.ArgumentParser(description="Run RAFT unit tests.")
-    parser.add_argument('--config', type=str, help='Path to the configuration file')
-    args, remaining_args = parser.parse_known_args()
-
-    if args.config:
-        print(f"Using config file: {args.config}")
-        # Load and set the configuration in the singleton
-        singleton.set_config(args.config)
-        # Load and process the config file if needed
-
-    # Run unit tests
-    unittest.main(argv=[sys.argv[0]] + remaining_args, exit=False)
 
 if __name__ == "__main__":
-    main()
+   RAFTUnitTestCase.main()
 
