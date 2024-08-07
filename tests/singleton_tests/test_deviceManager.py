c5e7de3ecf36e406d8f52b7d89e67f9d31cff098
TB-1993 <>
2024-08-07 10:00:36 +0100

    Fix #72: Added missing copyright header to test_deviceManager

--- tests/singleton_tests/test_deviceManager.py
+++ tests/singleton_tests/test_deviceManager.py
@@ -1,5 +1,36 @@
 #!/usr/bin/env python3
-
+#** ******************************************************************************
+# *
+# * If not stated otherwise in this file or this component's LICENSE file the
+# * following copyright and licenses apply:
+# *
+# * Copyright 2023 RDK Management
+# *
+# * Licensed under the Apache License, Version 2.0 (the "License");
+# * you may not use this file except in compliance with the License.
+# * You may obtain a copy of the License at
+# *
+# *
+# http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# * See the License for the specific language governing permissions and
+# * limitations under the License.
+# *
+#* ******************************************************************************
+#*
+#*   ** Project      : RAFT
+#*   ** @addtogroup  : tests
+#*   ** @file        : test_deviceManager.py
+#*   ** @date        : 07/08/20224
+#*   **
+#*   ** @brief : Tests the decodeManager class as part of the singleton.
+#*   **
+#*   ** The positive path for this test is to run it with the below arguments:
+#*   ** --config examples/configs/getting_started_rack_config.yml
+#* ******************************************************************************
 import os
 import sys
 
